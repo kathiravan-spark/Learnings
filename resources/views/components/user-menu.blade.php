e90a8109bb77247a680fadee5167f5fccaedc1cb
Kathiravan-spark <kathiravan@sparkouttech.com>
2022-04-24 14:28:04 +0530

    try

---
+++ resources/views/components/user-menu.blade.php
@@ -0,0 +1,4 @@
+<div>
+<a href="" class="btn btn-success mt-2 mb-2 mr-2">Buy</a>
+   <a href="" class="btn btn-info mt-2 mb-2 mr-2">Sell</a>
+</div>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                
+                <x-admin-menu/>
+                <x-user-menu/>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
